1e80d9ddfe1b478cc1fc1ed2ff69ada16c6fe6eb
Huseyin DOGAN <dogan.h@expoweb.fr>
2016-09-18 19:28:02 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// compiled dir/files are meant to be removed as soon as node handle async/await and es2015 modules
+// until then we'll do this way.
+var Router = require("./src-compiled/router");
+
+module.exports = new (Router.default)();
