a8e71b08ba31c206b190bb03a273888ec225fd19
Huseyin DOGAN <dogan.h@expoweb.fr>
2016-09-20 22:47:20 +0200

    Initial commit

--- src-compiled/saw-bootstrapper.js
+++ src-compiled/saw-bootstrapper.js
@@ -4,15 +4,35 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
 var _sawConfig = require("saw-config");
 
 var _sawConfig2 = _interopRequireDefault(_sawConfig);
+
+var _sawRouting = require("saw-routing");
+
+var _sawRouting2 = _interopRequireDefault(_sawRouting);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 class Bootstrapper {
     static bootstrap(app) {
-        //
+        app.use((() => {
+            var _ref = (0, _asyncToGenerator3.default)(function* (ctx, next) {
+                var route = _sawRouting2.default.match(ctx.request);
+
+                console.log(route.uri);
+
+                return next();
+            });
+
+            return function (_x, _x2) {
+                return _ref.apply(this, arguments);
+            };
+        })());
     }
 }
 
--- src/saw-bootstrapper.js
+++ src/saw-bootstrapper.js
@@ -1,10 +1,17 @@
 "use strict";
 
 import config from "saw-config";
+import router from "saw-routing";
 
 class Bootstrapper {
     static bootstrap(app) {
-        //
+        app.use(async (ctx, next) => {
+            var route = router.match(ctx.request);
+
+            console.log(route.uri);
+
+            return next();
+        });
     }
 }
 
